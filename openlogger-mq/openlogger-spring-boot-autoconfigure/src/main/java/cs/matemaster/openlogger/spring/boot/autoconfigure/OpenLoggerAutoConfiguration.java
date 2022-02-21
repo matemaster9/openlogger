83eb611e02852bf2dfa1ff9c31444666a2eb0b28
matemaster9 <68363002+matemaster9@users.noreply.github.com>
2022-02-21 13:29:27 +0800

    20220221 #comment openlogger-mq搭建

---
+++ openlogger-mq/openlogger-spring-boot-autoconfigure/src/main/java/cs/matemaster/openlogger/spring/boot/autoconfigure/OpenLoggerAutoConfiguration.java
@@ -0,0 +1,9 @@
+package cs.matemaster.openlogger.spring.boot.autoconfigure;
+
+/**
+ * @Description:
+ * @Author: MateMaster
+ * @Date: 2022/2/21 12:59
+ */
+public class OpenLoggerAutoConfiguration {
+}
---
+++ openlogger-mq/openlogger-spring-boot-autoconfigure/src/main/java/cs/matemaster/openlogger/spring/boot/autoconfigure/OpenLoggerProperties.java
@@ -0,0 +1,9 @@
+package cs.matemaster.openlogger.spring.boot.autoconfigure;
+
+/**
+ * @Description:
+ * @Author: MateMaster
+ * @Date: 2022/2/21 13:00
+ */
+public class OpenLoggerProperties {
+}
