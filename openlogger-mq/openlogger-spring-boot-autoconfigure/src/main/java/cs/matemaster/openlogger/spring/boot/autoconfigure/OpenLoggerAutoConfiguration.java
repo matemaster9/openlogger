c97ec88f7955caff8cfd61ff5d6b89244be18811
matemaster <matemaster@163.com>
2022-02-25 18:41:03 +0800

    去除mq

--- openlogger-mq/openlogger-spring-boot-autoconfigure/src/main/java/cs/matemaster/openlogger/spring/boot/autoconfigure/OpenLoggerAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cs.matemaster.openlogger.spring.boot.autoconfigure;
-
-/**
- * @Description:
- * @Author: MateMaster
- * @Date: 2022/2/21 12:59
- */
-public class OpenLoggerAutoConfiguration {
-}
--- openlogger-mq/openlogger-spring-boot-autoconfigure/src/main/java/cs/matemaster/openlogger/spring/boot/autoconfigure/OpenLoggerProperties.java
+++
@@ -1,9 +0,0 @@
-package cs.matemaster.openlogger.spring.boot.autoconfigure;
-
-/**
- * @Description:
- * @Author: MateMaster
- * @Date: 2022/2/21 13:00
- */
-public class OpenLoggerProperties {
-}
